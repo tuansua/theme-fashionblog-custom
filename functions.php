c0f0f61aae3cd4e7eb1784df294064f03a5eaf89
tuannt <tuannt@shopdunk.vn>
2020-03-18 17:32:26 +0700

    Update upload image auto add title, Alt, Description

--- functions.php
+++ functions.php
@@ -392,6 +392,7 @@ function get_image_src( $object, $field_name, $request ) {
   return $feat_img_array[0];
 }
 
+// Custom logo and background page login wordpress
 function tp_custom_logo() { ?>
     <style type="text/css">
         .login  {
@@ -417,6 +418,7 @@ function pridio_login_headerurl()
     return '24hdev.com';
 }
 
+// Custom limit upload image file size
 add_filter('wp_handle_upload_prefilter', 'ts_validate_file_upload');
 function ts_validate_file_upload($file) {
     // get imagetype of upload
@@ -445,6 +447,43 @@ function ts_validate_file_upload($file) {
 
         return $file;
     }
+}
+
+?>
+
+<?php 
+// Auto add title, Alt, description image upload
+add_action( 'add_attachment', 'my_set_image_meta_upon_image_upload' );
+function my_set_image_meta_upon_image_upload( $post_ID ) {
+
+	// Check if uploaded file is an image, else do nothing
+
+	if ( wp_attachment_is_image( $post_ID ) ) {
+
+		$my_image_title = get_post( $post_ID )->post_title;
+
+		// Sanitize the title:  remove hyphens, underscores & extra spaces:
+		$my_image_title = preg_replace( '%\s*[-_\s]+\s*%', ' ',  $my_image_title );
+
+		// Sanitize the title:  capitalize first letter of every word (other letters lower case):
+		$my_image_title = ucwords( strtolower( $my_image_title ) );
+
+		// Create an array with the image meta (Title, Caption, Description) to be updated
+		// Note:  comment out the Excerpt/Caption or Content/Description lines if not needed
+		$my_image_meta = array(
+			'ID'		=> $post_ID,			// Specify the image (ID) to be updated
+			'post_title'	=> $my_image_title,		// Set image Title to sanitized title
+			'post_excerpt'	=> $my_image_title,		// Set image Caption (Excerpt) to sanitized title
+			'post_content'	=> $my_image_title,		// Set image Description (Content) to sanitized title
+		);
+
+		// Set the image Alt-Text
+		update_post_meta( $post_ID, '_wp_attachment_image_alt', $my_image_title );
+
+		// Set the image meta (e.g. Title, Excerpt, Content)
+		wp_update_post( $my_image_meta );
+
+	} 
 }
 
 ?>
